ca5810ccaaf2d1693acdb241901cb33ee44d15d5
Emiliano Perez <emiperez.dev@gmail.com>
2023-12-22 18:29:01 -0600

    Create grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-function App() {
+import { Grid } from "@chakra-ui/react";
 
+function App() {
   return (
-    <div>Hello!</div>
-  )
+    <Grid
+      maxWidth="3xl"
+      bg="blue.100"
+      height="100vh"
+      margin='0 auto'
+    >
+
+    </Grid>
+  );
 }
 
-export default App
+export default App;
