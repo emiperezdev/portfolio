19a4dffed9cfcdcd488620fbb273df0507ed1eab
Emiliano Perez <emiperez.dev@gmail.com>
2023-12-30 20:26:11 -0600

    Add tooltip

--- src/components/MediaButtons.tsx
+++ src/components/MediaButtons.tsx
@@ -1,16 +1,19 @@
-import { IconButton, Button, Box } from "@chakra-ui/react";
+import { IconButton, Button, Box, Tooltip } from "@chakra-ui/react";
 import { GiBullseye } from "react-icons/gi";
 import { IoMdDocument } from "react-icons/io";
 
 export const MediaButtons = () => {
   return (
     <Box marginTop="10px">
-      <IconButton
-        colorScheme="green"
-        aria-label="Search database"
-        icon={<GiBullseye />}
-        fontSize="3xl"
-      />
+      <Tooltip hasArrow label="Looking for work">
+        <IconButton
+          colorScheme="green"
+          aria-label="Search database"
+          icon={<GiBullseye />}
+          fontSize="3xl"
+        />
+      </Tooltip>
+
       <Button
         marginLeft="20px"
         leftIcon={<IoMdDocument />}
