7d7c8fd3d6562f81ce5f609939c24063ff0d7102
Emiliano Perez <emiperez.dev@gmail.com>
2023-12-30 19:17:17 -0600

    Build border

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Grid } from "@chakra-ui/react";
 import { ColorModeIcon } from "./components/ColorModeIcon";
 import { Media } from "./components/Media";
+import { Border } from "./components/Border";
 
 function App() {
   return (
     <Grid maxWidth="735px" margin="0 auto" position='relative'>
-      {/* <Practice /> */}
       <ColorModeIcon />
       <Media />
+      <Border />
     </Grid>
   );
 }
---
+++ src/components/Border.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export const Border = () => {
+  return (
+    <Box marginTop='50px'>
+      <hr />
+    </Box>
+  );
+};
