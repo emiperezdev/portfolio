a4db9cbc8ac6f30d46aba603f1a3e94977b6ad4b
Emiliano Perez <emiperez.dev@gmail.com>
2024-01-15 12:48:30 -0600

    Download my cv by clicking resume button

--- src/components/media/MediaButtons.tsx
+++ src/components/media/MediaButtons.tsx
@@ -1,8 +1,18 @@
 import { IconButton, Button, Box, Tooltip, Icon } from "@chakra-ui/react";
 import { GiBullseye } from "react-icons/gi";
 import { IoMdDocument } from "react-icons/io";
+import cv from "../../assets/EmilianoPerezMurillo.pdf";
 
 export const MediaButtons = () => {
+  const downLoadCV = () => {
+    const link = document.createElement("a");
+    link.href = cv;
+    link.download = "EmilianoPerezMurillo_CV.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <Box marginTop="10px">
       <Tooltip hasArrow label="Looking for work">
@@ -21,8 +31,14 @@ export const MediaButtons = () => {
         colorScheme="purple"
         variant="solid"
         fontSize="md"
+        onClick={downLoadCV}
       >
-        <Icon marginBottom='2px' as={IoMdDocument} fontSize='20px' marginRight='13px'/>
+        <Icon
+          marginBottom="2px"
+          as={IoMdDocument}
+          fontSize="20px"
+          marginRight="13px"
+        />
         Resume
       </Button>
     </Box>
